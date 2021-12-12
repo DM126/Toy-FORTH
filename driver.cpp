fce2030a2e753623ee5e720ac58459284ae68976
DM126 <danny386126@gmail.com>
2021-12-11 23:26:08 -0600

    Add comments to driver

--- driver.cpp
+++ driver.cpp
@@ -3,12 +3,14 @@
 
 int main(int argc, char* argv[])
 {
+	//Validate the number of command line arguments
 	if (argc != 2)
 	{
 		std::cerr << "Error: Interpreter takes exactly 1 argument for the file to execute." << std::endl;
 		return 1;
 	}
 
+	//Convert the filename argument to a string and run the interpreter
 	std::string filename = std::string(argv[1]);
 	Interpreter interpreter(filename);
 
